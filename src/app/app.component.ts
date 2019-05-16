903dc3805ec0662a31978a4de088bfc5783f3f4a
eero94 <eero.oittinen@gmail.com>
2019-05-16 14:39:57 +0300

    Addes css stuff among other things.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'contact-webapp';
+  title = 'Komponentin otisikko on tässä!';
+message = '';
+
+onButtonClick() {
+  console.log('moikka');
+  this.message = 'heippa';
+
+}
 }
